54be774c30bbf9bd7083f4dfa1438e5f12e6c737
Michael McNelis <mjmcnelis@gmail.com>
2021-08-28 17:34:20 -0400

    Update parameters.py

--- parameters.py
+++ parameters.py
@@ -14,74 +14,74 @@ parameters = {'adaptive':               'ERK',
               'eps_root':               1.e-6,
               'interpolate':            False,
               'free_embedded':          None
-	     }
+             }
 
 #-------------------------------------------------------------------------------------------------------|
-#		 |											|
-#  		 |	adaptive time step algorithm (None, str)					|
-#    adaptive	 |											|
-#		 |	None, 'RKM', 'ERK', 'SDRK'							|
-#		 |											|
+#                |                                                                                      |
+#                |      adaptive time step algorithm (None, str)                                        |
+#    adaptive    |                                                                                      |
+#                |      None, 'RKM', 'ERK', 'SDRK'                                                      |
+#                |                                                                                      |
 #-------------------------------------------------------------------------------------------------------|
-#		 |											|
-#  		 |	code label of numerical method (str)						|
-#     method     |											|
-#		 |	E1, H2, RK4, F45, etc. (see butcher_table.py, multistep_table.py)		|
-#		 | 											|
+#                |                                                                                      |
+#                |      code label of numerical method (str)                                            |
+#     method     |                                                                                      |
+#                |      E1, H2, RK4, F45, etc. (see butcher_table.py, multistep_table.py)               |
+#                |                                                                                      |
 #-------------------------------------------------------------------------------------------------------|
-#		 |											|
-#  		 |	initial time step (float)							|
-#   dt_initial   |											|
-#		 |	note: if adaptive != None, then it's a proposed value				|
-#		 |											|
+#                |                                                                                      |
+#                |      initial time step (float)                                                       |
+#   dt_initial   |                                                                                      |
+#                |      note: if adaptive != None, then it's a proposed value                           |
+#                |                                                                                      |
 #-------------------------------------------------------------------------------------------------------|
-#		 |											|
-#   max_steps    |	max number of time steps (int)							|
-#		 |											|
+#                |                                                                                      |
+#   max_steps    |      max number of time steps (int)                                                  |
+#                |                                                                                      |
 #-------------------------------------------------------------------------------------------------------|
-#		 |											|
-#      eps       |	tolerance parameter for error control (float)					|
-#		 |											|
+#                |                                                                                      |
+#      eps       |      tolerance parameter for error control (float)                                   |
+#                |                                                                                      |
 #-------------------------------------------------------------------------------------------------------|
-#		 |											|
+#                |                                                                                      |
 #  		 |	type of norm used for error control (None, int, np.inf)				|
 #      norm      |											|
 #		 |	None, 1, np.inf									|
-#		 |											|
+#                |                                                                                      |
 #-------------------------------------------------------------------------------------------------------|
-#		 |											|
+#                |                                                                                      |
 #     dt_min     |	min time step allowed (float)						 	|
-#		 |											|
+#                |                                                                                      |
 #-------------------------------------------------------------------------------------------------------|
-#		 |											|
+#                |                                                                                      |
 #     dt_max     |	max time step allowed (float)						   	|
-#		 |											|
+#                |                                                                                      |
 #-------------------------------------------------------------------------------------------------------|
-#		 |											|
+#                |                                                                                      |
 #       	 |	root-finder method for implicit routines (str)					|
 #      root      |											|
 #		 |	'fixed_point', 'newton', 'newton_fast'						|
-#		 |											|
+#                |                                                                                      |
 #-------------------------------------------------------------------------------------------------------|
-#		 |											|
+#                |                                                                                      |
 # max_iterations |	max number of root-finder iterations (int >= 1)					|
-#		 |											|
+#                |                                                                                      |
 #-------------------------------------------------------------------------------------------------------|
-#		 |											|
+#                |                                                                                      |
 #    eps_root    |	tolerance parameter for root-finder convergence (float)				|
-#		 |											|
+#                |                                                                                      |
 #-------------------------------------------------------------------------------------------------------|
-#		 |											|
+#                |                                                                                      |
 # 		 |	option to interpolate numerical solution for dense output (bool)		|
 #  interpolate   |											|
 #		 |	True, False									|
-#		 |											|
+#                |                                                                                      |
 #-------------------------------------------------------------------------------------------------------|
-#		 |											|
+#                |                                                                                      |
 # 		 |	option to replace embedded pair of explicit Runge-Kutta scheme with a free pair	|
 # free_embedded  |											|
 #		 |	None, 1 (Euler), 2 (generic 2nd order)		(e.g. Fehlberg 4(1))		|
-#		 |											|
+#                |                                                                                      |
 #-------------------------------------------------------------------------------------------------------|
 
 
